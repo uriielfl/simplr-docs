c500831ff8a2fbe46823f5ee7fcd36f94bf73d70
Uriel Francisco Libano <uriellibano2009@gmail.com>
2024-07-03 18:49:22 -0300

    fix: adjust base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/simplr-docs/',
   plugins: [react(), checker({ typescript: false })],
   resolve: {
     alias: {
